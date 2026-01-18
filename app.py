e7af33f25bcc47ac1330527a2a5c97899d5300a0
Gopal Prajapati <gp240606@gmail.com>
2026-01-18 13:42:18 +0530

    Remove space in predicted salary output message

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ gender = st.selectbox('Gender', encoders['Gender'].classes_)
 education_level = st.selectbox('Education Level', encoders['Education Level'].classes_)
 job = st.selectbox('Job Title', encoders['Job Title'].classes_)
 experience = st.number_input('Years of Experience', 0.0, 40.0, 2.0)
-
 # Create DataFrame
 df = pd.DataFrame({
     'Age': [age],
@@ -23,11 +22,10 @@ df = pd.DataFrame({
     'Job Title': [job],
     'Years of Experience': [experience]
 })
-
 # Predict
 if st.button("Predict Salary"):
     for col in encoders:
         if col in df.columns:
             df[col] = encoders[col].transform(df[col])
     prediction = model.predict(df)
-    st.success(f"💰 Predicted Salary: ₹ {prediction[0]:,.2f}")
+    st.success(f"💰 Predicted Salary:{prediction[0]:,.2f}")
